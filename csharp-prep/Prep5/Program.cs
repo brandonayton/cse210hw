b5b7d341798412cc34f60e315eb5c71a10644b8a
brandonayton <131309488+brandonayton@users.noreply.github.com>
2023-11-18 19:01:06 -0700

    prep 5 finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Call DisplayWelcome function
+        DisplayWelcome();
+
+        // Call PromptUserName function and save the return value
+        string userName = PromptUserName();
+
+        // Call PromptUserNumber function and save the return value
+        int userNumber = PromptUserNumber();
+
+        // Call SquareNumber function with the userNumber and save the squared result
+        int squaredNumber = SquareNumber(userNumber);
+
+        // Call DisplayResult function with the userName and squaredNumber
+        DisplayResult(userName, squaredNumber);
+    }
+
+    // Function to display a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    // Function to prompt the user for their name and return it
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    // Function to prompt the user for their favorite number and return it as an integer
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    // Function to square a given number and return the result
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    // Function to display the user's name and the squared number
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
