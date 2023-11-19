d4a83adbab02872a654289510dc26eb9c852a757
brandonayton <131309488+brandonayton@users.noreply.github.com>
2023-11-18 18:59:57 -0700

    prep 3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int loop = 0;
+        Console.WriteLine($"What is the magic number? {number}");
+        
+        while (loop == 0)
+        {
+            Console.WriteLine("What is your guess? ");
+            string answer = Console.ReadLine();
+            int guess = int.Parse(answer);
+
+            if (guess == number)
+            {
+                loop = 1;
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine("You guessed it!");
     }
 }
